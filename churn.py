367446ee88ec3bd7e268109eab269c8892446763
rouxstuff <scheiblereduardo@gmail.com>
2026-09-13 23:47:58 -0300

    começando as analises

--- churn.py
+++ churn.py
@@ -1,9 +1,8 @@
 # bibliotecas
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import warnings
-warnings.filterwarnings('ignore')
 
-data = pd.read_csv('/content/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+
+data = pd.read_csv('content/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+
+print(data.shape)
+print(data.columns)
