8141da8f7a16cc49e50f3f15d9a8cdf142a85478
Roux <scheiblereduardo@gmail.com>
2026-09-13 23:14:54 -0300

    #

---
+++ churn.py
@@ -0,0 +1,9 @@
+# bibliotecas
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+data = pd.read_csv('/content/WA_Fn-UseC_-Telco-Customer-Churn.csv')
